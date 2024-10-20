04bc05ed88ac0ddfac7b8d489080014e4280809b
Daniel <Danielmikhh@gmail.com>
2024-10-20 21:45:18 +0200

    Добавьте файлы проекта.

---
+++ IrisChat/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IrisChat.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
